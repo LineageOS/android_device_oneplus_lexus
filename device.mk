e2b295715fdaf1a085800e680d4341c8c2bc46bf
dianlujitao <dianlujitao@lineageos.org>
2025-09-09 19:58:45 +0530

    Revert "lexus: Override udfps sensor location for lexus"
    reason for revert: https://review.lineageos.org/c/LineageOS/android_hardware_oplus/+/487595
    
    This reverts commit 941cc1ac4b34b473a19968b1a63161e77cf4a6ad.
    
    Change-Id: Id3f20a19f7f83eb62f02a6f9a3c6e553f8b7a5dd

--- device.mk
+++ device.mk
@@ -26,10 +26,6 @@ PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0-service \
     hwservicemanager
 
-# Init
-PRODUCT_PACKAGES += \
-    init.oplus.lexus.rc
-
 # LiveDisplay
 $(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
 
