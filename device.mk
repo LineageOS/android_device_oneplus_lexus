3e11100619ca88c9dc3540fc1b602686a8a2b9ed
grepfox <grepfox@tutamail.com>
2025-11-08 23:40:13 +0530

    lexus: Make Pluskey an assistant key
    gpio-keys.kl keylayouts kanged from oxygenos with assist key mapped
    
    Change-Id: Ida9c1a5c2d5613002cd2e9ea46d7434472046f11

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_PACKAGES += \
     OPlusSystemUIResTarget \
     OPlusWifiResTarget
 
+# Pluskey
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
+
 # Regional properties
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/odm/etc/24881/build.EU.prop:$(TARGET_COPY_OUT_ODM)/etc/24881/build.EU.prop \
