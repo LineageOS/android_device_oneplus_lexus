f50a4a687f2c34ae44672c294749830493fd4b22
grepfox <grepfox@tutamail.com>
2025-12-30 10:04:58 +0000

    lexus: Initial device tree conversion
    Change-Id: I7efe506768b821bd1a37259dfd564078e711f07c

--- lineage_lexus.mk
+++ lineage_lexus.mk
@@ -7,24 +7,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from avalon device
-$(call inherit-product, device/oneplus/avalon/device.mk)
+# Inherit from lexus device
+$(call inherit-product, device/oneplus/lexus/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_avalon
-PRODUCT_DEVICE := avalon
+PRODUCT_NAME := lineage_lexus
+PRODUCT_DEVICE := lexus
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := CPH2661
+PRODUCT_MODEL := CPH2707
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BP2A.250605.015 1763267221232 release-keys" \
-    BuildFingerprint=OnePlus/CPH2661IN/OP5E93L1:16/UKQ1.231108.001/U.R4T2.a1a83f_16e546a_16e5468:user/release-keys \
-    DeviceName=OP5E93L1 \
-    DeviceProduct=CPH2661 \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1762873368969 release-keys" \
+    BuildFingerprint=OnePlus/CPH2707IN/OP6131L1:16/UKQ1.231108.001/V.R4T2.202511112342:user/release-keys \
+    DeviceName=OP6131L1 \
+    DeviceProduct=CPH2707 \
     SystemDevice=OP5E93L1 \
-    SystemName=CPH2661
+    SystemName=CPH2707
