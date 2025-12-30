d3c2fb844e00ffdd36fc2feb46e4e7ddef249609
grepfox <grepfox@tutamail.com>
2025-12-30 10:04:58 +0000

    lexus: Initial device tree conversion
    Change-Id: I7efe506768b821bd1a37259dfd564078e711f07c

--- extract-files.py
+++ extract-files.py
@@ -79,7 +79,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'avalon',
+    'lexus',
     'oneplus',
     namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
